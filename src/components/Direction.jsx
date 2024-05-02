3fc9dd5706f93ee412edf79f32cc2305a5b4fffe
Purna Shrestha <crush9918@gmail.com>
2024-05-02 15:02:04 +0545

    feat: Add janaki temple description and a scroll reveal and parallax

---
+++ src/components/Direction.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NearbyMapComponent from "./ui/NearbyMapComponent";
+
+const Direction = () => {
+  return (
+    <>
+      <NearbyMapComponent />
+    </>
+  );
+};
+
+export default Direction;
--- src/components/Janaki.jsx
+++ src/components/Janaki.jsx
@@ -1,61 +1,24 @@
 import React from "react";
+import ScrollReveal from "./ui/ScrollReveal";
 
 const Janaki = () => {
   return (
     <>
       <div
-        className={`relative h-screen bg-[url('https://www.mithilayatriniwas.com/template/cms/images/things-to-do.png')] bg-center bg-cover bg-fixed`}
+        // className={`relative min-h-screen bg-[url('https://3.bp.blogspot.com/_dFHgYO4fOVk/S4LXrpqQ-JI/AAAAAAAAAA0/-Nh2V4Xv0dE/s1600/FILE0561.JPG')] bg-cover bg-top bg-fixed`}
+        // className={`relative min-h-screen bg-[url('https://scontent.fktm10-1.fna.fbcdn.net/v/t39.30808-6/307644824_6560528780646526_8981790166085048237_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=yU5dbIdT3gEQ7kNvgH7njhL&_nc_ht=scontent.fktm10-1.fna&oh=00_AfAsy8OQYgQFc3-rggatBdgD5MOEw8A2yibQxlvE7HBfBA&oe=66390858')] bg-cover bg-top bg-fixed`}
+        className={`relative min-h-screen bg-[url('/src/assets/janakiTemple.jpg')] bg-cover bg-top bg-fixed`}
       >
-        <div className="absolute bg-black/70 inset-0 z-0" />
+        <div className="absolute bg-black/40 inset-0 z-0" />
         <section className="z-10 text-custom-white">
-          <div className="container">
-            <div className="space-y-6 text-center ">
-              <h3 className="w-3/4 mx-auto text-3xl leading-relaxed">
-                The Janaki Temple is a Hindu temple in Janakpur, Nepal,
-                dedicated to the Hindu goddess Sita.
-              </h3>
-              <p className="w-11/12 mx-auto text-lg text-center">
-                It is an example of mixed Islam and Hindu Nepali architecture.
-                It is often considered the most important model of Mughal
-                architecture in Nepal. The Mandir (Temple) is popularly known as
-                the Nau Lakha Mandir (meaning "nine lakhs"). The cost for the
-                construction of the temple was about the same amount of money:
-                rupees nine lakhs or nine hundred thousand, hence the name.
-                Queen Vrisha Bhanu of Tikamgarh built the temple in 1910 AD. The
-                site was designated as a UNESCO tentative site in 2008.
-              </p>
-              <p>
-                This seat of Hindu pilgrimage is a sprawling collection of
-                temples, ashrams, images and inscriptions raised over the
-                centuries.
-              </p>
-              <p>
-                In 1657, a golden statue of the Goddess Sita was found at the
-                very spot, and Sita is said to have lived there. The legend said
-                it that it was built on the holy site where Sannyasi
-                Shurkishordas had found the images of Goddess Sita. In fact,
-                Shurkishordas was the founder of modern Janakpur and the great
-                saint and poet who preached about the Sita Upasana (also called
-                Sita Upanishad) philosophy. Legend has claimed it that King
-                Janak(Seeradhwaj) performed the worship of Shiva-Dhanus on this
-                site.
-              </p>
-              <p>
-                This important Hindu pilgrimage spot is one of the most visited
-                spots in Nepal and attracts thousands of Hindu pilgrims from
-                India and across the world due to its religious importance. The
-                temple is even more resplendent on religious feats and special
-                occasions Ram Nawami, Vivaha Panchami, Dashain and Tihar when
-                people flock here in even greater number.
-              </p>
-              <p>
-                In the shadow of this magnificent temple complex and along the
-                banks of the lakes Hotel Mithila a 3-star spiritual theme hotel
-                came into existence to offer the finest hospitality and an
-                experience of a lifetime.
-              </p>
-            </div>
-          </div>
+          <ScrollReveal para="The Janaki Temple is a Hindu temple in Janakpur, Nepal, dedicated to the Hindu goddess Sita." />
+          <ScrollReveal para="It is an example of mixed Islam and Hindu Nepali architecture. It is often considered the most important model of Mughal architecture in Nepal." />
+          <ScrollReveal para="The Mandir (Temple) is popularly known as the Nau Lakha Mandir (meaning 'nine lakhs'). The cost for the construction of the temple was about rupees nine lakhs, hence the name." />
+          <ScrollReveal para="Queen Vrisha Bhanu of Tikamgarh built the temple in 1910 AD. The site was designated as a UNESCO tentative site in 2008." />
+          <ScrollReveal para="This seat of Hindu pilgrimage is a sprawling collection of temples, ashrams, images and inscriptions raised over the centuries." />
+          <ScrollReveal para="This important Hindu pilgrimage spot is one of the most visited spots in Nepal and attracts thousands of Hindu pilgrims from across the world. " />
+          <ScrollReveal para="The temple is even more resplendent on religious feats and special occasions Ram Nawami, Vivaha Panchami, Dashain and Tihar when people flock here in even greater number." />
+          <ScrollReveal para="In the shadow of this magnificent temple complex and along the banks of the lakes Hotel Mithila a 3-star spiritual theme hotel came into existence to offer the finest hospitality and an experience of a lifetime." />
         </section>
       </div>
     </>
