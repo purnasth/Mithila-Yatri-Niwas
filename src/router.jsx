5c5143e3fbb296aa1bf7c8c735fdf6772e32690d
Purna Shrestha <crush9918@gmail.com>
2024-05-05 20:33:46 +0545

    hold: Dine page

--- src/router.jsx
+++ src/router.jsx
@@ -5,12 +5,16 @@ import GalleryPage from "./pages/GalleryPage";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/404";
 import Visit from "./pages/Visit";
+import DinePage from "./pages/DinePage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/accommodation", element: <Accommodation /> },
+  { path: "/dine", element: <DinePage /> },
   { path: "/gallery", element: <GalleryPage /> },
   { path: "/contact", element: <Contact /> },
   { path: "/places-to-visit", element: <Visit /> },
   { path: "*", element: <NotFound /> },
 ]);
+
+// DineComponent.jsx DinePage.jsx
