08d5712c7984841ddbf26ea948713ff9d5e1ae3a
Purna Shrestha <crush9918@gmail.com>
2024-05-05 17:48:10 +0545

    feat: Update the neraby map location with embedded map

--- src/components/ui/NearbyMapComponent.jsx
+++ src/components/ui/NearbyMapComponent.jsx
@@ -22,6 +22,36 @@ const NearbyMapComponent = () => {
       distance: "4.4km",
       map: "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d14254.776502965655!2d85.91469190244248!3d26.722217028176132!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x39ec414cff7d8891%3A0xb626d6ab90442f61!2sMithila%20Yatri%20Niwas%2C%20Janakpur!3m2!1d26.732046399999998!2d85.9219169!4m5!1s0x39ec407231231aa1%3A0xab58893c40e1b844!2sJanakpur%20Airport%2C%20Janakpur!3m2!1d26.711423!2d85.9238786!5e0!3m2!1sen!2snp!4v1714642074225!5m2!1sen!2snp",
     },
+    {
+      name: "Ram Mandir",
+      distance: "900m",
+      map: "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d3563.453335365392!2d85.92191032521768!3d26.72990587675653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x39ec414cff7d8891%3A0xb626d6ab90442f61!2sMithila%20Yatri%20Niwas%2C%20Janakpur!3m2!1d26.732046399999998!2d85.9219169!4m5!1s0x39ec6aa7178b2099%3A0x409b43633a40308c!2sRam%20Janki%20Mandir%2C%20Janakpur%2045600!3m2!1d26.728321299999998!2d85.9270694!5e0!3m2!1sen!2snp!4v1714909635965!5m2!1sen!2snp",
+    },
+    {
+      name: "Ram Sita Vivah Mandap",
+      distance: "350m",
+      map: "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1781.7041110251569!2d85.92215268864092!3d26.731346194188955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x39ec414cff7d8891%3A0xb626d6ab90442f61!2sMithila%20Yatri%20Niwas%2C%20Janakpur!3m2!1d26.732046399999998!2d85.9219169!4m5!1s0x39ec4004272d21b3%3A0x9d53eac3f112f3cb!2sShree%20Ram%20Janaki%20Biwah%20Mandap%2C%20PWJF%2B9XQ%2C%20Janakpur%2045600!3m2!1d26.7308986!2d85.9249777!5e0!3m2!1sen!2snp!4v1714910137389!5m2!1sen!2snp",
+    },
+    {
+      name: "Jaleshwar",
+      distance: "2.5km",
+      map: "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d7126.93387405459!2d85.926136792178!3d26.729471607027143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x39ec414cff7d8891%3A0xb626d6ab90442f61!2sMithila%20Yatri%20Niwas%2C%20Janakpur!3m2!1d26.732046399999998!2d85.9219169!4m5!1s0x39ec3fe9d84d90b5%3A0xa9a97ee9216804d1!2sJaleshwar%2C%20Janakpur!3m2!1d26.727146599999998!2d85.9406745!5e0!3m2!1sen!2snp!4v1714910233145!5m2!1sen!2snp",
+    },
+    {
+      name: "Ganga Sagar",
+      distance: "1km",
+      map: "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d3563.4389448151937!2d85.92354142528828!3d26.730365326756306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x39ec414cff7d8891%3A0xb626d6ab90442f61!2sMithila%20Yatri%20Niwas%2C%20Janakpur!3m2!1d26.732046399999998!2d85.9219169!4m5!1s0x39ec4007f3532d19%3A0x42a8839df3243056!2sGanga%20Sagar%2C%20Janakpur!3m2!1d26.728684299999998!2d85.9301352!5e0!3m2!1sen!2snp!4v1714642006000!5m2!1sen!2snp",
+    },
+    {
+      name: "Dhanusha Dham",
+      distance: "3.7km",
+      map: "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d28502.863123107087!2d85.90349842046005!3d26.748910661658073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x39ec414cff7d8891%3A0xb626d6ab90442f61!2sMithila%20Yatri%20Niwas%2C%20Janakpur!3m2!1d26.732046399999998!2d85.9219169!4m5!1s0x39ec6bae3cea9a5f%3A0x5797e7156d6c5ad8!2sDhanusha%20Dham%20To%20Way%2C%20Janakpur!3m2!1d26.7627621!2d85.9293596!5e0!3m2!1sen!2snp!4v1714910403541!5m2!1sen!2snp",
+    },
+    {
+      name: "Ratna Sagar Temple",
+      distance: "2.3km",
+      map: "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d7126.454603238048!2d85.91324694217909!3d26.737121657008814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x39ec414cff7d8891%3A0xb626d6ab90442f61!2sMithila%20Yatri%20Niwas%2C%20Janakpur!3m2!1d26.732046399999998!2d85.9219169!4m5!1s0x39ec6aa9c2e7013f%3A0x991420bd3f1e596b!2sRatna%20Sagar%20Temple%2C%20Janakpur!3m2!1d26.740272299999997!2d85.9161153!5e0!3m2!1sen!2snp!4v1714910523542!5m2!1sen!2snp",
+    },
   ];
 
   const [isMapOpen, setIsMapOpen] = useState(true); // Initially true to show the map
