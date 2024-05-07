53a6771300fa105b176a9c2064de068a902aba09
Purna Shrestha <crush9918@gmail.com>
2024-05-07 13:14:14 +0545

    Add dine amenities

--- src/pages/DinePage.jsx
+++ src/pages/DinePage.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import PackageSlider from "../components/ui/PackageSlider";
 
-import room1 from "../assets/rooms/room1.webp";
-import room2 from "../assets/rooms/room2.webp";
-import room3 from "../assets/rooms/room3.webp";
 import BreadCrumb from "../components/ui/BreadCrumb";
 import RoomAmenities from "../components/ui/RoomAmenities";
 import Reservation from "../components/ui/Reservation";
 
-import { IoBed, IoPricetagSharp } from "react-icons/io5";
-import { IoMdResize } from "react-icons/io";
 import { GiBowlOfRice } from "react-icons/gi";
 import { IoMdClock } from "react-icons/io";
 
@@ -17,27 +12,23 @@ import PackageList from "../components/PackageList";
 
 import dine1 from "../assets/dine/dine.webp";
 
-import {
-  FaWifi,
-  FaTv,
-  FaBed,
-  FaBath,
-  FaSwimmingPool,
-  FaUtensils,
-  FaParking,
-  FaAirFreshener,
-  FaCoffee,
-  FaFireAlt,
-  FaChild,
-  FaWheelchair,
-} from "react-icons/fa";
-import { MdTableRestaurant } from "react-icons/md";
-import { GiSlippers } from "react-icons/gi";
-import { TbDeviceLandlinePhone } from "react-icons/tb";
-import { GiTowel } from "react-icons/gi";
-import { FaBottleWater } from "react-icons/fa6";
-
 import { HiMiniUserGroup } from "react-icons/hi2";
+
+import {
+  FaUtensils,
+  FaCoffee,
+  FaGlassCheers,
+  FaChalkboardTeacher,
+  FaMusic,
+  FaWineBottle,
+  FaBeer,
+  FaTemperatureHigh,
+  FaClock,
+  FaWater,
+} from "react-icons/fa";
+import { MdTableChart } from "react-icons/md";
+import { GiCandleLight } from "react-icons/gi";
+import { GiCookingPot, GiHotMeal } from "react-icons/gi";
 
 const DinePage = () => {
   const dineCategories = [
@@ -104,23 +95,19 @@ const DinePage = () => {
     },
   ];
 
-  const roomAmenities = [
-    { icon: <FaWifi />, title: "Free Wi-Fi" },
-    { icon: <FaTv />, title: "Flat-screen TV" },
-    { icon: <FaBed />, title: "Comfortable Bed" },
-    { icon: <FaBath />, title: "Private Bathroom" },
-    { icon: <MdTableRestaurant />, title: "Furniture" },
-    { icon: <FaUtensils />, title: "Dining Area" },
-    // { icon: <FaParking />, title: "Free Parking" },
-    { icon: <FaAirFreshener />, title: "Air Conditioning" },
-    { icon: <FaCoffee />, title: "Coffee Maker" },
-    // { icon: <FaFireAlt />, title: "Fireplace" },
-    // { icon: <FaChild />, title: "Kids Play Area" },
-    // { icon: <FaWheelchair />, title: "Wheelchair Accessible" },
-    { icon: <GiSlippers />, title: "Slippers" },
-    { icon: <TbDeviceLandlinePhone />, title: "Telephone" },
-    { icon: <GiTowel />, title: "Towel Service" },
-    { icon: <FaBottleWater />, title: "Water Bottle" },
+  const dineAmenities = [
+    { icon: <MdTableChart />, title: "Ample Seating" },
+    { icon: <FaUtensils />, title: "Cutlery Provided" },
+    { icon: <FaGlassCheers />, title: "Beverage Service" },
+    { icon: <FaChalkboardTeacher />, title: "Menu Display" },
+    { icon: <FaMusic />, title: "Background Music" },
+    { icon: <FaCoffee />, title: "Coffee Station" },
+    { icon: <FaWineBottle />, title: "Wine Selection" },
+    { icon: <FaBeer />, title: "Beer on Tap" },
+    { icon: <GiCandleLight />, title: "Ambiance Lighting" },
+    { icon: <FaTemperatureHigh />, title: "Air Conditioner" },
+    { icon: <GiCookingPot />, title: "Live Cooking" },
+    { icon: <GiHotMeal />, title: "Hot Meal Counter" },
   ];
 
   return (
@@ -149,7 +136,7 @@ const DinePage = () => {
         <PackageList contents={dineCategories} />
 
         <RoomAmenities
-          amenities={roomAmenities}
+          amenities={dineAmenities}
           amenitiesTitle="Dine Amenities"
         />
 
