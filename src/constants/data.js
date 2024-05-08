a9ff05e4b542360c08153819eb6b5abe6cc7d4d2
Purna Shrestha <crush9918@gmail.com>
2024-05-08 10:18:43 +0545

    dynamic: accommodation page

--- src/constants/data.js
+++ src/constants/data.js
@@ -86,7 +86,7 @@ export {
 
 import {
   FaInstagram,
-  FaTripadvisor,
+  // FaTripadvisor,
   FaWhatsapp,
   FaUtensils,
   FaCoffee,
@@ -96,8 +96,6 @@ import {
   FaWineBottle,
   FaBeer,
   FaTemperatureHigh,
-  FaClock,
-  FaWater,
   FaWifi,
   FaTv,
   FaAirFreshener,
@@ -105,12 +103,16 @@ import {
   FaVideo,
   FaMapPin,
   FaPrint,
+  FaBed,
+  FaBath,
 } from "react-icons/fa";
+import { FaBottleWater } from "react-icons/fa6";
 import {
   TbPhone,
   TbBrandFacebook,
   TbPencilQuestion,
   TbCalendarEvent,
+  TbDeviceLandlinePhone,
 } from "react-icons/tb";
 import { HiOutlineMail } from "react-icons/hi";
 
@@ -130,8 +132,10 @@ import {
   GiCookingPot,
   GiHotMeal,
   GiTowel,
+  GiSlippers,
 } from "react-icons/gi";
-import { IoMdClock } from "react-icons/io";
+import { IoMdClock, IoMdResize } from "react-icons/io";
+import { IoBed, IoPricetagSharp } from "react-icons/io5";
 import { HiMiniUserGroup } from "react-icons/hi2";
 import { PiProjectorScreenFill } from "react-icons/pi";
 import { BsFillProjectorFill } from "react-icons/bs";
@@ -481,6 +485,207 @@ export const hallContents = [
               "Please adhere to the designated capacity limits for safety and comfort.",
             ],
           },
+        ],
+      },
+    ],
+  },
+];
+
+export const accommodationContents = [
+  {
+    title: "Accommodation",
+    subheading: "",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In pariatur odit reiciendis quae quod, accusamus quos, consectetur ipsum cum excepturi rem ea labore perspiciatis animi qui nesciunt a deserunt voluptas alias aliquam vero expedita enim ab assumenda! Beatae, mollitia qui unde temporibus dolorum architecto maiores, ut corrupti nemo facilis quos?",
+    roomsCategories: [
+      {
+        id: 1,
+        title: "Deluxe Room",
+        description:
+          "Comfortable and homely for relaxation with view of our garden, the lake or the Janaki Temple some distance away. All our 18 Deluxe Rooms are designed keeping in mind your needs but nothing in excess. These rooms are opulently designed and decorated for comfort of our guests.",
+        image: [
+          {
+            src: room1,
+            alt: "Deluxe Room",
+          },
+        ],
+        router: "/accommodation/deluxe-room",
+        booking:
+          "https://www.mithilayatriniwas.com/result.php?hotel_code=gBRaLW",
+        informations: [
+          {
+            title: "Informations",
+            data: {
+              "Total Rooms": { icon: IoBed, value: "18 rooms" },
+              Price: { icon: IoPricetagSharp, value: "Starting USD 40" },
+              "Room Size": { icon: IoMdResize, value: "205 sq. ft." },
+              "Rate plan": { icon: GiBowlOfRice, value: "Bed & Breakfast" },
+              "Check-in/out": { icon: IoMdClock, value: "1pm - 12 noon" },
+            },
+          },
+        ],
+        facilities: [
+          {
+            title: "Facilities",
+            content: [
+              "Welcome drink (non-alcoholic) on arrival.",
+              "Tea/Coffee maker in room with sachet.",
+              "Iron and Ironing board in the room on request.",
+              "Free internet access throughout the stay in the room.",
+              "Two mineral water bottles (1000ml) every day in the room.",
+            ],
+          },
+        ],
+        policies: [
+          {
+            title: "Policies",
+            content: [
+              "Child 0 - 4 yrs. sharing parent's room free of charge on same plan as adults.",
+              "Child age 5 - 10 yrs. sharing parent's room will be charged 50% adult meal rate.",
+              "Breakfast - USD $8/ INR 500.",
+            ],
+          },
+        ],
+        roomAmenities: [
+          { icon: FaWifi, title: "Free Wi-Fi" },
+          { icon: FaTv, title: "Flat-screen TV" },
+          { icon: FaBed, title: "Comfortable Bed" },
+          { icon: FaBath, title: "Private Bathroom" },
+          { icon: MdTableRestaurant, title: "Furniture" },
+          { icon: FaUtensils, title: "Dining Area" },
+          { icon: FaAirFreshener, title: "Air Conditioning" },
+          { icon: FaCoffee, title: "Coffee Maker" },
+          { icon: GiSlippers, title: "Slippers" },
+          { icon: TbDeviceLandlinePhone, title: "Telephone" },
+          { icon: GiTowel, title: "Towel Service" },
+          { icon: FaBottleWater, title: "Water Bottle" },
+        ],
+      },
+      {
+        id: 2,
+        title: "Executive Room",
+        description:
+          "Comfortable and homely for relaxation with view of our garden, the lake or the Janaki Temple some distance away. All our 10 Executive Rooms are designed keeping in mind your needs but nothing in excess. These rooms are opulently designed and decorated for comfort of our guests.",
+        image: [
+          {
+            src: room2,
+            alt: "Deluxe Room",
+          },
+        ],
+        router: "/accommodation/executive-suite",
+        booking:
+          "https://www.mithilayatriniwas.com/result.php?hotel_code=gBRaLW",
+        informations: [
+          {
+            title: "Informations",
+            data: {
+              "Total Rooms": { icon: IoBed, value: "10 rooms" },
+              Price: { icon: IoPricetagSharp, value: "Starting USD 50" },
+              "Room Size": { icon: IoMdResize, value: "215 sq. ft." },
+              "Rate plan": { icon: GiBowlOfRice, value: "Bed & Breakfast" },
+              "Check-in/out": { icon: IoMdClock, value: "1pm - 12 noon" },
+            },
+          },
+        ],
+        facilities: [
+          {
+            title: "Facilities",
+            content: [
+              "Welcome drink (non-alcoholic) on arrival.",
+              "Tea/Coffee maker in room with sachet.",
+              "Iron and Ironing board in the room on request.",
+              "Free internet access throughout the stay in the room.",
+              "Two mineral water bottles (1000ml) every day in the room.",
+            ],
+          },
+        ],
+        policies: [
+          {
+            title: "Policies",
+            content: [
+              "Child 0 - 4 yrs. sharing parent's room free of charge on same plan as adults.",
+              "Child age 5 - 10 yrs. sharing parent's room will be charged 50% adult meal rate.",
+              "Breakfast - USD $8/ INR 500.",
+            ],
+          },
+        ],
+        roomAmenities: [
+          { icon: FaWifi, title: "Free Wi-Fi" },
+          { icon: FaTv, title: "Flat-screen TV" },
+          { icon: FaBed, title: "Comfortable Bed" },
+          { icon: FaBath, title: "Private Bathroom" },
+          { icon: MdTableRestaurant, title: "Furniture" },
+          { icon: FaUtensils, title: "Dining Area" },
+          { icon: FaAirFreshener, title: "Air Conditioning" },
+          { icon: FaCoffee, title: "Coffee Maker" },
+          { icon: GiSlippers, title: "Slippers" },
+          { icon: TbDeviceLandlinePhone, title: "Telephone" },
+          { icon: GiTowel, title: "Towel Service" },
+          { icon: FaBottleWater, title: "Water Bottle" },
+        ],
+      },
+      {
+        id: 3,
+        title: "Suite Room",
+        description:
+          "Comfortable and homely for relaxation with view of our garden, the lake or the Janaki Temple some distance away. All our 2 Suite Rooms are designed keeping in mind your needs but nothing in excess. These rooms are opulently designed and decorated for comfort of our guests.",
+        image: [
+          {
+            src: room3,
+            alt: "Deluxe Room",
+          },
+        ],
+        router: "/accommodation/suite-room",
+        booking:
+          "https://www.mithilayatriniwas.com/result.php?hotel_code=gBRaLW",
+        informations: [
+          {
+            title: "Informations",
+            data: {
+              "Total Rooms": { icon: IoBed, value: "2 rooms" },
+              Price: { icon: IoPricetagSharp, value: "Starting USD 60" },
+              "Room Size": { icon: IoMdResize, value: "215 sq. ft." },
+              "Rate plan": { icon: GiBowlOfRice, value: "Bed & Breakfast" },
+              "Check-in/out": { icon: IoMdClock, value: "1pm - 12 noon" },
+            },
+          },
+        ],
+        facilities: [
+          {
+            title: "Facilities",
+            content: [
+              "Fruits Basket and VIP Amenities on room.",
+              "Welcome drink (non-alcoholic) on arrival.",
+              "Tea/Coffee maker in room with sachet.",
+              "Iron and Ironing board in the room on request.",
+              "Free internet access throughout the stay in the room.",
+              "Two mineral water bottles (1000ml) every day in the room.",
+            ],
+          },
+        ],
+        policies: [
+          {
+            title: "Policies",
+            content: [
+              "Child 0 - 4 yrs. sharing parent's room free of charge on same plan as adults.",
+              "Child age 5 - 10 yrs. sharing parent's room will be charged 50% adult meal rate.",
+              "Breakfast - USD $8/ INR 500.",
+            ],
+          },
+        ],
+        roomAmenities: [
+          { icon: FaWifi, title: "Free Wi-Fi" },
+          { icon: FaTv, title: "Flat-screen TV" },
+          { icon: FaBed, title: "Comfortable Bed" },
+          { icon: FaBath, title: "Private Bathroom" },
+          { icon: MdTableRestaurant, title: "Furniture" },
+          { icon: FaUtensils, title: "Dining Area" },
+          { icon: FaAirFreshener, title: "Air Conditioning" },
+          { icon: FaCoffee, title: "Coffee Maker" },
+          { icon: GiSlippers, title: "Slippers" },
+          { icon: TbDeviceLandlinePhone, title: "Telephone" },
+          { icon: GiTowel, title: "Towel Service" },
+          { icon: FaBottleWater, title: "Water Bottle" },
         ],
       },
     ],
