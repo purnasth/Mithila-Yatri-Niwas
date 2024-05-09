b28ca2b93f289f829848cfb968a79b8870ee5e2b
Purna Shrestha <crush9918@gmail.com>
2024-05-09 15:49:18 +0545

    feat: Make the navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { RiArrowDropDownLine } from "react-icons/ri";
-
+import { IoIosMenu, IoMdClose } from "react-icons/io";
 import { navLinks, logo, Button, Socials } from "../constants/data";
 
 const Navbar = () => {
   const [openDropdownId, setOpenDropdownId] = useState(null);
   const [activePage, setActivePage] = useState(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
 
   const toggleDropdown = (id, event) => {
     event.preventDefault();
@@ -22,9 +23,13 @@ const Navbar = () => {
     }
   }, [navLinks]);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <header className="relative h-24 w-full flex items-center justify-between px-6 z-50 bg-custom-black/20 backdrop-blur-sm bg-gradient-to-t from-custom-white/40 to-transparent">
+      <header className="hidden relative h-12 md:h-24 w-full md:flex items-center justify-between px-2 md:px-6 z-50 bg-custom-black/20 backdrop-blur-sm bg-gradient-to-t from-custom-white/40 to-transparent">
         <Socials />
 
         <h1>
@@ -32,7 +37,7 @@ const Navbar = () => {
             <img
               src={logo}
               alt="Mithila Yatri Niwas"
-              className=" w-32 h-24 p-1 object-contain transition-linear hover:scale-110 drop-shadow-lg"
+              className="w-20 h-12 md:w-32 md:h-24 p-1 object-contain transition-linear hover:scale-110 drop-shadow-lg"
             />
           </a>
         </h1>
@@ -43,8 +48,36 @@ const Navbar = () => {
         />
       </header>
 
-      <nav className="sticky top-0 h-12 bg-custom-black/30 backdrop-blur-sm border-t border-b border-custom-white/50 text-custom-white  drop-shadow-lg z-50">
-        <ul className="flex items-center flex-wrap justify-center gap-12 h-full font-medium cursor-pointer tracking-wider">
+      <nav className="sticky top-0 h-12 bg-custom-black/30 backdrop-blur-sm md:border-t border-b border-custom-white/50 text-custom-white drop-shadow-lg z-50">
+        <div className="flex md:hidden items-center justify-between pr-4">
+          <a href="/">
+            <img
+              src={logo}
+              alt="Mithila Yatri Niwas"
+              className="w-20 h-12 p-1 object-contain transition-linear drop-shadow-lg bg-custom-white"
+            />
+          </a>
+          <button className="relative p-2 " onClick={toggleMenu}>
+            <IoIosMenu
+              className={`text-2xl absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 transition-linear duration-300 ${
+                isMenuOpen ? "scale-100" : "scale-0"
+              }`}
+            />
+            <IoMdClose
+              className={`text-2xl absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 transition-linear duration-300 ${
+                isMenuOpen ? "scale-0" : "scale-100"
+              }`}
+            />
+          </button>
+        </div>
+
+        <ul
+          className={`p-6 md:p-0 bg-alt-logo-clr md:bg-transparent flex flex-col md:flex-row items-center justify-center gap-6 md:gap-12 h-auto md:h-full font-medium cursor-pointer tracking-wider transition-linear ${
+            isMenuOpen ? "scale-x-0 md:scale-x-100" : "scale-x-100"
+          }`}
+        >
+          <div className="md:hidden absolute inset-0 size-full bg-[url('/src/assets/graphics/flowers.png')] bg-contain bg-repeat mix-blend-multiply opacity-20 -z-20" />
+
           {navLinks.map((link) => (
             <li
               key={link.id}
