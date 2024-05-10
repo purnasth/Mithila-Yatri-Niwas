5694c45b605a6fdaad328a1f7a728d0c5fa605fe
Purna Shrestha <crush9918@gmail.com>
2024-05-10 15:22:01 +0545

    feat: Make the package pages dynamic

--- src/components/ui/RoomAmenities.jsx
+++ src/components/ui/RoomAmenities.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RoomAmenities = ({ amenities, amenitiesTitle }) => {
   return (
-    <div className="container py-12 my-32 p-0">
+    <div className="container py-12 md:my-32 p-0">
       <h4 className="text-3xl text-center my-8">{amenitiesTitle}</h4>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-12 mt-24">
         {amenities.map((amenity, index) => (
@@ -16,7 +16,7 @@ const RoomAmenities = ({ amenities, amenitiesTitle }) => {
                 className="w-8 h-8 text-alt-logo-clr"
               />
             )}
-            <p>{amenity.title}</p>
+            <p className="text-xs md:text-xs xl:text-base">{amenity.title}</p>
           </div>
         ))}
       </div>
--- src/pages/DinePage.jsx
+++ src/pages/DinePage.jsx
@@ -13,21 +13,23 @@ const DinePage = () => {
 
   return (
     <>
-      <div className="responsive-banner">
+      <div className="responsive-banner relative overflow-hidden">
         <PackageSlider
           contents={dineCategories}
-          sizeClassName="w-full h-screen"
+          sizeClassName="w-full h-full lg:h-screen"
         />
         <BreadCrumb page="Dine" />
       </div>
 
       <section className="bg-custom-white">
         <div className="text-center mb-16 container max-w-5xl mx-auto">
-          <h2 className="text-3xl">{title}</h2>
-          <p className="text-base text-gray-500 mt-4">{description}</p>
+          <h2 className="text-2xl sm:text-3xl">{title}</h2>
+          <p className="text-sm md:text-base text-gray-500 mt-4">
+            {description}
+          </p>
           <div className="mt-8 w-full">
             <a
-              href=""
+              href="#menu"
               className="font-title text-custom-white bg-alt-logo-clr hover:bg-logo-clr px-6 py-2 rounded-full transition-linear group"
             >
               View Menu
