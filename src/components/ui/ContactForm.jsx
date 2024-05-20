2384ba6f4104151d512f047656952e89459a01d4
Purna Shrestha <crush9918@gmail.com>
2024-05-20 17:15:39 +0545

    feat: Create captcha on contact

--- src/components/ui/ContactForm.jsx
+++ src/components/ui/ContactForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import { IoMdCloseCircle, IoMdCheckmarkCircle, IoMdSync } from "react-icons/io";
+import { FormButton } from "../../constants/data";
 
 const ContactForm = ({ contactFormFields }) => {
   const [formData, setFormData] = useState(
@@ -8,6 +10,7 @@ const ContactForm = ({ contactFormFields }) => {
       return acc;
     }, {})
   );
+  const [recaptchaToken, setRecaptchaToken] = useState(null);
   const [formStatus, setFormStatus] = useState(null);
 
   const handleChange = (e) => {
@@ -18,19 +21,29 @@ const ContactForm = ({ contactFormFields }) => {
     });
   };
 
+  const handleRecaptchaChange = (token) => {
+    setRecaptchaToken(token);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      // Validate form data
-      const isValid = validateForm(formData);
-      if (!isValid) {
-        // Display error message if form data is invalid
-        setFormStatus("error");
-        setTimeout(() => setFormStatus(null), 3000); // Clear error after 3 seconds
-        return;
-      }
 
-      // Form data is valid, proceed with form submission
+    // Validate form data
+    const isValid = validateForm(formData);
+    if (!isValid) {
+      setFormStatus("error");
+      setTimeout(() => setFormStatus(null), 3000);
+      return;
+    }
+
+    // Validate reCAPTCHA
+    if (!recaptchaToken) {
+      setFormStatus("recaptchaError");
+      setTimeout(() => setFormStatus(null), 3000);
+      return;
+    }
+
+    try {
       setFormStatus("loading");
       const response = await fetch(
         "https://mithilayatriniwas.com/enquery_mail_react.php",
@@ -39,33 +52,32 @@ const ContactForm = ({ contactFormFields }) => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(formData),
+          body: JSON.stringify({
+            ...formData,
+            "g-recaptcha-response": recaptchaToken,
+          }),
         }
       );
 
       if (response.ok) {
         const data = await response.json();
         console.log("Success:", data);
-        // Display success message
         setFormStatus("success");
-        setTimeout(() => setFormStatus(null), 3000); // Clear success after 3 seconds
+        setTimeout(() => setFormStatus(null), 3000);
       } else {
         console.error("Error:", response.statusText);
-        // Display error message
         setFormStatus("error");
-        setTimeout(() => setFormStatus(null), 3000); // Clear error after 3 seconds
+        setTimeout(() => setFormStatus(null), 3000);
       }
     } catch (error) {
       console.error("Error:", error);
-      // Display error message
       setFormStatus("error");
-      setTimeout(() => setFormStatus(null), 3000); // Clear error after 3 seconds
+      setTimeout(() => setFormStatus(null), 3000);
     }
   };
 
   // Function to validate form data
   const validateForm = (formData) => {
-    // Example validation: Ensure all fields are not empty
     for (const key in formData) {
       if (formData[key].trim() === "") {
         return false;
@@ -112,22 +124,21 @@ const ContactForm = ({ contactFormFields }) => {
             </div>
           ))}
         </div>
-        <div className="mt-16 flex items-center justify-center">
-          <button
-            type="submit"
-            className="bg-alt-logo-clr text-white py-2 px-4 rounded-full hover:bg-opacity-80 transition duration-300 ease-in-out"
-          >
-            Submit
-          </button>
+        <div className="mt-8 flex items-center justify-between flex-col md:flex-row gap-12">
+          <ReCAPTCHA
+            // sitekey="6LcsfeEpAAAAAEq9Yyl0jxseAXX3rAO0LGIc2_Qu"
+            sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" //test captcha key
+            onChange={handleRecaptchaChange}
+          />
+          <FormButton title="Submit" />
         </div>
       </form>
-      {/* Render form status message */}
       {formStatus && (
         <div
           className={`${
             formStatus === "success"
               ? "bg-green-200 text-green-700"
-              : formStatus === "error"
+              : formStatus === "error" || formStatus === "recaptchaError"
               ? "bg-red-200 text-red-700"
               : "bg-yellow-100 text-yellow-700"
           } font-bold max-w-sm mx-auto fixed top-16 left-4 right-4 p-4 rounded-md shadow-lg flex items-center gap-4 z-[99] transition-opacity duration-300 ease-in-out transform ${
@@ -136,11 +147,14 @@ const ContactForm = ({ contactFormFields }) => {
         >
           {formStatus === "success" && <IoMdCheckmarkCircle />}
           {formStatus === "error" && <IoMdCloseCircle />}
+          {formStatus === "recaptchaError" && <IoMdCloseCircle />}
           {formStatus === "loading" && <IoMdSync className="animate-spin" />}
           {formStatus === "success"
             ? " Your message has been sent successfully!"
             : formStatus === "error"
             ? "Unable to send message. Please try again."
+            : formStatus === "recaptchaError"
+            ? "Please complete the reCAPTCHA."
             : "Loading..."}
         </div>
       )}
