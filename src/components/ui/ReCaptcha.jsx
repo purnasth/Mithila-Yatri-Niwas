0160563192845f93eb11d5eb8019b4dd8bf68874
Purna Shrestha <crush9918@gmail.com>
2024-06-11 16:35:41 +0545

    feat: add real captcha

--- src/components/ui/ReCaptcha.jsx
+++ src/components/ui/ReCaptcha.jsx
@@ -4,8 +4,8 @@ import ReCAPTCHA from "react-google-recaptcha";
 const ReCaptcha = ({ onChange }) => {
   return (
     <ReCAPTCHA
-      // sitekey="6LcsfeEpAAAAAEq9Yyl0jxseAXX3rAO0LGIc2_Qu"
-      sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // replace with your actual sitekey
+      sitekey="6LcsfeEpAAAAAEq9Yyl0jxseAXX3rAO0LGIc2_Qu"
+      // sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // replace with your actual sitekey
       onChange={onChange}
     />
   );
