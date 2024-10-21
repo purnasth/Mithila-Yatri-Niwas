06c39b9553f9846c089bb0ee9dbdbefa96f962c8
Purna Shrestha <crush9918@gmail.com>
2024-10-21 16:11:19 +0545

    api: Article page

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,42 +1,9 @@
-// import React from "react";
-// import { useParams } from "react-router-dom";
-// import { articlePageContents } from "../constants/data";
-// import NotFound from "./404";
-
-// const ArticlePage = () => {
-//   const { slug } = useParams();
-//   const article = articlePageContents[slug];
-
-//   if (!article) {
-//     return <NotFound />;
-//   }
-
-//   return (
-//     <div className="container mx-auto px-4 py-8">
-//       <div className="space-y-6 text-center text-custom-black">
-//         <h2 className="w-full md:w-3/4 mx-auto text-xl md:text-3xl leading-relaxed">
-//           {article.title}
-//         </h2>
-//         <h3 className="text-lg md:text-2xl">{article.subtitle}</h3>
-//         <p className="w-full md:w-11/12 mx-auto text-sm md:text-lg text-justify md:text-center text-custom-black/80">
-//           {article.description}
-//         </p>
-//       </div>
-
-//       <img src={article.image} alt={article.title} className="w-full mb-4" />
-//       <p className="text-lg mb-4">{article.description}</p>
-//     </div>
-//   );
-// };
-
-// export default ArticlePage;
-
 import React from "react";
 import { useParams } from "react-router-dom";
-import { articlePageContents } from "../constants/data";
 import NotFound from "./404";
+import { withDataFetching } from "../constants/data";
 
-const ArticlePage = () => {
+const ArticlePage = ({ data: articlePageContents }) => {
   const { slug } = useParams();
   const article = articlePageContents[slug];
 
@@ -51,4 +18,15 @@ const ArticlePage = () => {
   );
 };
 
-export default ArticlePage;
+const transformArticlePageContents = (data) => {
+  const safeData = (code) => {
+    const func = new Function(code + "return articlePageContents;");
+    return func();
+  };
+  return safeData(data);
+};
+
+export default withDataFetching(
+  "https://mithilayatriniwas.com/api/api_article.php",
+  transformArticlePageContents
+)(ArticlePage);
